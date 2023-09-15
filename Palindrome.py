4b40262c0e9d2503ca004d9bc8766300ecd2ed59
Winterflakes <sakshirs1234@gmail.com>
2023-09-15 12:50:46 +0530

    first commit

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+# Given a Integer array A[] of n elements. 
+# Your task is to complete the function PalinArray. 
+# Which will return 1 if all the elements of the Array are palindrome otherwise it will return 0.
+
+def PalinArray(arr ,n):
+    # Code here
+    for i in arr:
+        num = str(i)
+        num1 = num[::-1]
+        if num1!=num:
+            return 0
+    return 1
